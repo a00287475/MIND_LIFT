a18708495f9eb7f67aa1cd7feb6d8791c5af293b
a00287475 <a00287475@mycambrian.ca>
2025-03-27 12:24:22 -0400

    Added some more comments

--- MIND_LIFT/Model/User.cs
+++ MIND_LIFT/Model/User.cs
@@ -10,12 +10,12 @@ namespace MIND_LIFT.Model
     // This class is used to Represent a user in the system with authentication details and account-related information
     public class User
     {
-        public string UserId { get; set; }     // Gives a unique ID for the user.
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
-        public bool IsActive { get; set; }
+        public string UserId { get; set; }     // Gives a unique ID for the user from Firebase.
+        public string Email { get; set; }     // User Email
+        public string PasswordHash { get; set; } // Hashed Password
+        public DateTime CreatedDate { get; set; } // Account creation timestamp
+        public DateTime UpdatedDate { get; set; } // Last Updated timestamp
+        public bool IsActive { get; set; } // Indicates whether the user is active or not
 
 
     }
